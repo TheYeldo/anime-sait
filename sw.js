f423807c1b9d6d7fe0d3f1ecd635ba40f2d32b80
TheYeldo <eldosegimbaev606@gmail.com>
2026-06-28 20:13:17 +0500

    Update site

--- sw.js
+++ sw.js
@@ -1,9 +1,9 @@
-const CACHE_NAME = "taytlo-shell-20260628-temp-launch-1";
+const CACHE_NAME = "taytlo-shell-20260628-poster-fit-1";
 const APP_SHELL = [
   "./",
   "./index.html",
-  "./styles.css?v=20260628-temp-launch-1",
-  "./script.js?v=20260628-temp-launch-1",
+  "./styles.css?v=20260628-poster-fit-1",
+  "./script.js?v=20260628-poster-fit-1",
   "./site.webmanifest",
   "./favicon.svg",
   "./runtime-config.js?v=20260622-1",
